5df79376ff12b78a83f4fec8371886b493cf5655
vlad <microgub@outlook.com>
2017-04-26 21:03:14 +0500

    index page has done

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,12 @@
 $(function(){
-    
+    var btn_close = document.getElementById('close');
+    var doge = document.getElementById('doge');
+    btn_close.addEventListener('click',wow);
+    function wow(){
+        doge.style.display = 'flex';
+        setTimeout(wowclose,4000)
+    }
+    function wowclose(){
+        doge.style.display = 'none';
+    }
 });
